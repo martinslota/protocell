0a5282144b250f570530dc00bc14cf918ab921b1
Martin Slota <martin.slota@gmail.com>
2019-09-02 23:40:16 +0200

    Extracted common functionality in the code generator

--- src/generator/generator.ml
+++ src/generator/generator.ml
@@ -4,28 +4,74 @@ module Generated_code = Shared_types.Generated_code
 
 module Code = struct
   type t =
-    | Sequence of t list
     | Line of string
+    | Block of block
+
+  and block = {
+    indented : bool;
+    contents : t list;
+  }
 
   let line string = Line string
 
-  let sequence sequence = Sequence sequence
+  let block ?(indented = true) contents = Block {indented; contents}
+
+  let lines strings = strings |> List.map ~f:line |> block
+
+  let make_list elements =
+    block [line "["; elements |> List.map ~f:(fun e -> e ^ ";") |> lines; line "]"]
+
+  let make_record ?(prefix = "") elements =
+    block
+      [
+        line @@ prefix ^ "{";
+        elements |> List.map ~f:(fun e -> e ^ ";") |> lines;
+        line "}";
+      ]
+
+  let make_record_type type_name fields =
+    block
+      [
+        type_name |> Printf.sprintf "type %s = {" |> line;
+        fields
+        |> List.map ~f:(fun (field_name, field_type) ->
+               Printf.sprintf "%s : %s;" field_name field_type)
+        |> lines;
+        line "}";
+      ]
+
+  let make_let name type_annotation code =
+    block [Printf.sprintf "let %s : %s =" name type_annotation |> line; code]
+
+  let make_match expression cases =
+    let cases =
+      List.map cases ~f:(fun (pattern, code) ->
+          [pattern |> Printf.sprintf "| %s ->" |> line; block [code]])
+    in
+    [line "match"; expression; line "with"] :: cases |> List.concat |> block
+
+  let make_lambda argument body =
+    block [argument |> Printf.sprintf "fun %s ->" |> line; body]
+
+  let make_module name items =
+    let items = List.intersperse items ~sep:(line "") in
+    block ~indented:false
+    @@ List.concat
+         [[name |> Printf.sprintf "module %s = struct" |> line]; items; [line "end"]]
+
+  let make_file items =
+    let items = List.intersperse items ~sep:(line "") in
+    block ~indented:false items
 
   let emit code =
-    let rec append buffer indent = function
+    let rec append ~indent buffer = function
       | Line string -> List.iter [indent; string; "\n"] ~f:(Buffer.add_string buffer)
-      | Sequence sequence ->
-          let bigger_indent = Printf.sprintf "  %s" indent in
-          List.iter sequence ~f:(fun each ->
-              let indent =
-                match each with
-                | Line _ -> indent
-                | Sequence _ -> bigger_indent
-              in
-              append buffer indent each)
+      | Block {indented; contents} ->
+          let indent = if indented then Printf.sprintf "  %s" indent else indent in
+          List.iter contents ~f:(fun each -> append buffer ~indent each)
     in
     let buffer = Base.Buffer.create 4096 in
-    append buffer "" code; Buffer.contents buffer
+    append buffer ~indent:"" code; Buffer.contents buffer
 end
 
 let generate_message : Protobuf.Message.t -> Code.t =
@@ -42,134 +88,114 @@ let generate_message : Protobuf.Message.t -> Code.t =
     | String -> "string"
     | Int32 -> "int"
   in
-  let fields_code =
+  let type_declaration =
     fields
     |> List.map ~f:(fun Protobuf.Field.{name; data_type; _} ->
-           Printf.sprintf "%s : %s;" name (type_to_ocaml_type data_type) |> Code.line)
-    |> Code.sequence
+           name, type_to_ocaml_type data_type)
+    |> Code.make_record_type "t"
   in
   let serialize_code =
-    let declaration = "let serialize : t -> string =" |> Code.line in
-    let definition =
-      let arguments =
-        fields
-        |> List.map ~f:(fun Protobuf.Field.{name; _} -> name)
-        |> String.concat ~sep:"; "
-      in
-      String.concat ~sep:" " ["fun"; "{"; arguments; "}"; "->"] |> Code.line
-    in
-    let field_info =
+    Code.make_let "serialize" "t -> string"
+    @@
+    let argument =
       fields
-      |> List.map ~f:(fun Protobuf.Field.{name; number; data_type} ->
-             Printf.sprintf
-               "%d, F'.%s, F'.encode_%s %s;"
-               number
-               (type_to_string data_type)
-               (type_to_snake_case data_type)
-               name
-             |> Code.line)
-      |> Code.sequence
+      |> List.map ~f:(fun Protobuf.Field.{name; _} -> name)
+      |> String.concat ~sep:"; "
+      |> Printf.sprintf "{ %s }"
     in
     let body =
-      ["[" |> Code.line; field_info; "]" |> Code.line; "|> M'.serialize" |> Code.line]
-      |> Code.sequence
+      let serialization_items =
+        fields
+        |> List.map ~f:(fun Protobuf.Field.{name; number; data_type} ->
+               Printf.sprintf
+                 "%d, F'.%s, F'.encode_%s %s"
+                 number
+                 (type_to_string data_type)
+                 (type_to_snake_case data_type)
+                 name)
+        |> Code.make_list
+      in
+      Code.(block [serialization_items; line "|> M'.serialize"])
     in
-    [declaration; [definition; body] |> Code.sequence] |> Code.sequence
+    Code.make_lambda argument body
   in
   let deserialize_code =
-    let declaration =
-      "let deserialize : string -> (t, F'.decoding_error) result =" |> Code.line
-    in
-    let definition = "fun input' ->" |> Code.line in
+    Code.make_let "deserialize" "string -> (t, F'.decoding_error) result"
+    @@
+    let argument = "input'" in
     let decoder_declarations =
       fields
       |> List.map ~f:(fun Protobuf.Field.{name; data_type; _} ->
              Printf.sprintf
                "let %s = F'.%s_decoder () in"
                name
-               (type_to_snake_case data_type)
-             |> Code.line)
-      |> Code.sequence
-    in
-    let deserialize_args =
-      fields
-      |> List.map ~f:(fun Protobuf.Field.{name; number; _} ->
-             Printf.sprintf "%d, F'.consume %s;" number name |> Code.line)
-      |> Code.sequence
-    in
-    let result_fields =
-      fields
-      |> List.map ~f:(fun Protobuf.Field.{name; _} ->
-             Printf.sprintf "%s = F'.value %s;" name name |> Code.line)
-      |> Code.sequence
+               (type_to_snake_case data_type))
+      |> Code.lines
     in
     let result_match =
-      [
-        "match M'.deserialize input'" |> Code.line;
-        ["[" |> Code.line; deserialize_args; "]" |> Code.line] |> Code.sequence;
-        "with" |> Code.line;
-        "| Ok () ->" |> Code.line;
-        [["Ok {" |> Code.line; result_fields; "}" |> Code.line] |> Code.sequence]
-        |> Code.sequence;
-        "| Error _ as error -> error" |> Code.line;
-      ]
-      |> Code.sequence
+      Code.(
+        make_match
+          (block
+             [
+               argument |> Printf.sprintf "M'.deserialize %s" |> line;
+               fields
+               |> List.map ~f:(fun Protobuf.Field.{name; number; _} ->
+                      Printf.sprintf "%d, F'.consume %s" number name)
+               |> make_list;
+             ])
+          [
+            ( "Ok ()",
+              fields
+              |> List.map ~f:(fun Protobuf.Field.{name; _} ->
+                     Printf.sprintf "%s = F'.value %s" name name)
+              |> make_record ~prefix:"Ok " );
+            "Error _ as error", lines ["error"];
+          ])
     in
-    let body = [decoder_declarations; result_match] |> Code.sequence in
-    [declaration; [definition; body] |> Code.sequence] |> Code.sequence
+    let body = [decoder_declarations; result_match] |> Code.block ~indented:false in
+    Code.make_lambda argument body
   in
   let stringify_code =
-    let declaration = "let stringify : t -> string =" |> Code.line in
-    let definition =
-      let arguments =
-        fields
-        |> List.map ~f:(fun Protobuf.Field.{name; _} -> name)
-        |> String.concat ~sep:"; "
-      in
-      String.concat ~sep:" " ["fun"; "{"; arguments; "}"; "->"] |> Code.line
-    in
-    let field_info =
+    Code.make_let "stringify" "t -> string"
+    @@
+    let argument =
       fields
-      |> List.map ~f:(fun Protobuf.Field.{name; data_type; _} ->
-             Printf.sprintf
-               {|"%s", F'.stringify_%s %s;|}
-               name
-               (type_to_snake_case data_type)
-               name
-             |> Code.line)
-      |> Code.sequence
+      |> List.map ~f:(fun Protobuf.Field.{name; _} -> name)
+      |> String.concat ~sep:"; "
+      |> Printf.sprintf "{ %s }"
     in
     let body =
-      ["[" |> Code.line; field_info; "]" |> Code.line; "|> M'.stringify" |> Code.line]
-      |> Code.sequence
+      let serialization_items =
+        fields
+        |> List.map ~f:(fun Protobuf.Field.{name; data_type; _} ->
+               Printf.sprintf
+                 {|"%s", F'.stringify_%s %s|}
+                 name
+                 (type_to_snake_case data_type)
+                 name)
+        |> Code.make_list
+      in
+      Code.(block [serialization_items; line "|> M'.stringify"])
     in
-    [declaration; [definition; body] |> Code.sequence] |> Code.sequence
+    Code.make_lambda argument body
   in
-  let module_ =
-    Code.
-      [
-        name |> Printf.sprintf "module %s = struct" |> line;
-        [line "type t = {"; fields_code; line "}"] |> sequence;
-        serialize_code;
-        deserialize_code;
-        stringify_code;
-        line "end";
-      ]
-    |> Code.sequence
-  in
-  module_
+  Code.make_module
+    name
+    [type_declaration; serialize_code; deserialize_code; stringify_code]
 
 let generate_file : Protobuf.File.t -> Generated_code.File.t =
  fun {name; messages} ->
-  {
-    name;
-    contents =
-      "module F' = Runtime.Field\nmodule M' = Runtime.Message\n"
-      ^ (messages
-        |> List.map ~f:generate_message
-        |> List.map ~f:Code.emit
-        |> String.concat ~sep:"");
-  }
+  let contents =
+    Code.(
+      make_file
+        [
+          line "module F' = Runtime.Field";
+          line "module M' = Runtime.Message";
+          List.map messages ~f:generate_message |> block ~indented:false;
+        ])
+    |> Code.emit
+  in
+  {name; contents}
 
 let generate_files : Protobuf.t -> Generated_code.t =
  fun {files} -> List.map ~f:generate_file files
